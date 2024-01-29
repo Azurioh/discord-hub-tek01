2e9aaefacd8b660ce9e29e20d39c7861049423b7
Azurioh <alan.cunin@epitech.eu>
2024-01-29 20:28:04 +0100

    Fix undefined function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import loadClient from "./utils/loadClient.js";
 import { config } from "dotenv";
 
-config();
-const client = loadClient();
+async function Main()
+{
+    config();
+    const client = loadClient();
 
-if (client === null) {
-    console.error("An error has occured, closing the program...");
-    exit(1);
-} else {
-    if (!client.config.TOKEN) {
-        console.error("No token entered in the .env file, closing the program...");
-        exit(1);
+    if (client === null) {
+        console.error("An error has occured, closing the program...");
+        return;
+    } else {
+        if (!client.config.TOKEN) {
+            console.error("No token entered in the .env file, closing the program...");
+            return;
+        }
+        await client.login(client.config.TOKEN);
     }
-    client.login(client.config.TOKEN);
 }
+
+(async () => Main())();
