dd1c12105fdabdab931dd7b652239bd380f17e21
Azurioh <alan.cunin@epitech.eu>
2024-02-01 14:34:21 +0100

    (+) music commands handling

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -22,10 +22,22 @@ function interactionCreate(interaction, client)
     if (interaction.guild) {
         interaction.user = interaction.guild.members.cache.get(interaction.user.id);
     }
+    const userVoiceChannel = interaction.user.voice.channel;
+    const botVoiceChannel = interaction.guild.members.me.voice.channel;
     fileToLoad = getFileToLoad(interaction, client);
     if (!fileToLoad) {
         interaction.reply({ content: "Cette action n'est pas disponible.", ephemeral: true });
         return;
+    }
+    if (fileToLoad.music) {
+        if (!userVoiceChannel) {
+            interaction.reply({ content: "Tu dois être dans un channel vocal afin d'utiliser des commandes de musiques", ephemeral: true });
+            return;
+        }
+        if (botVoiceChannel && userVoiceChannel !== botVoiceChannel) {
+            interaction.reply({ content: `Je joue déjà de la musique dans le channel <#${botVoiceChannel}>`, ephemeral: true });
+            return;
+        }
     }
     try {
         fileToLoad.run(interaction, client)
